06127c8c6407ee996cec25d5a029c51127cbf361
Maria Micek <mmicek16@wp.pl>
2019-03-31 15:26:43 +0200

    Local storage, filter and filter buttons added

--- script.js
+++ script.js
@@ -1,19 +1,26 @@
 class toDoList {
     constructor(selector){
         this.container = document.querySelector(selector) || document.body
-        this.tasks = []
+        this.tasks = JSON.parse(localStorage.getItem('to-do-list')) || []
         this.render()
+    }
+
+    saveTask(){
+        localStorage.setItem('to-do-list', JSON.stringify(this.tasks))
     }
 
     addTask(text){
         const newTask = new Task(text)
         this.tasks = this.tasks.concat(newTask.task)
+        this.showAll()
         this.render()
+        this.saveTask()
     }    
 
     removeTask(index){
         this.tasks.splice(index, 1)
         this.render()
+        this.saveTask()
     }
 
     toggleTask(element){
@@ -23,12 +30,56 @@ class toDoList {
             element.isTaskCompleted = false
         }
         this.render()
+        this.saveTask()
+    }
+
+    filterTasks(searchedItem){
+        const searchedItemMod = searchedItem.toLowerCase().replace(/ /g,'')
+        this.tasks.filter(element => {
+            if (element.text.toLowerCase().replace(/ /g,'').includes(searchedItemMod)){
+                element.isTaskShowed = true
+            } else {
+                element.isTaskShowed = false
+            }
+        })
+        this.render()
+    }
+
+    showAll(){
+        this.tasks.forEach(element => element.isTaskShowed = true)
+        this.render()
+    }
+
+    showCompleted(){
+        this.tasks.filter(element => {
+            if (element.isTaskCompleted){
+                element.isTaskShowed = true
+            } else {
+                element.isTaskShowed = false
+            }
+        })
+        this.render()
+    }
+
+    showUncompleted(){
+        this.tasks.filter(element => {
+            if (element.isTaskCompleted){
+                element.isTaskShowed = false
+            } else {
+                element.isTaskShowed = true
+            }
+        })
+        this.render()
     }
 
     render(){
         this.container.innerHTML =''
         this.renderFrom()
+        this.renderFilterInput()
         this.renderTask()
+        this.renderButtonShowAll()
+        this.renderButtonShowCompleted()
+        this.renderButtonShowUncompleted()
     }
 
     renderFrom(){
@@ -49,12 +100,61 @@ class toDoList {
         this.container.appendChild(buttonAdd)
     }
 
+    renderFilterInput(){
+        const input = document.createElement('input')
+        const buttonSearch = document.createElement('button')
+
+        input.setAttribute('placeholder', 'Find your tasks')
+        buttonSearch.innerText = 'SEARCH'
+        
+        buttonSearch.addEventListener(
+            'click',
+            () => {
+                if (input.value) this.filterTasks(input.value)
+            }
+        )
+
+        this.container.appendChild(input)
+        this.container.appendChild(buttonSearch)
+    }
+
+    renderButtonShowAll(){
+        const buttonShowAll = document.createElement('button')
+        buttonShowAll.innerText = 'ALL'
+        buttonShowAll.addEventListener(
+            'click',
+            () => this.showAll()
+        )
+        this.container.appendChild(buttonShowAll)
+    }
+
+    renderButtonShowCompleted(){
+        const buttonShowCompleted = document.createElement('button')
+        buttonShowCompleted.innerText = 'COMPLETED'
+        buttonShowCompleted.addEventListener(
+            'click',
+            () => this.showCompleted()
+        )
+        this.container.appendChild(buttonShowCompleted)
+    }
+
+    renderButtonShowUncompleted(){
+        const buttonShowUncompleted = document.createElement('button')
+        buttonShowUncompleted.innerText = 'UNCOMPLETED'
+        buttonShowUncompleted.addEventListener(
+            'click',
+            () => this.showUncompleted()
+        )
+        this.container.appendChild(buttonShowUncompleted)
+    }
+
     renderTask(){
         this.tasks.forEach( (element, index) => {
             const div = document.createElement('div')
             const span = document.createElement('span')
             const buttonDelete = document.createElement('button')
 
+            div.className = 'task'
             span.innerText = `${index + 1}. ${element.text}`
             if (element.isTaskCompleted) span.style.textDecoration = 'line-through'
             buttonDelete.innerText = 'DELETE'
@@ -69,6 +169,9 @@ class toDoList {
                 () => this.toggleTask(element)
             )
 
+            if (element.isTaskShowed) div.style.display = 'block'
+            if (!element.isTaskShowed) div.style.display = 'none'
+
             this.container.appendChild(div)
             div.appendChild(span)
             div.appendChild(buttonDelete)
@@ -81,7 +184,8 @@ class Task {
     constructor(text){
         this.task = {
             text: text,
-            isTaskCompleted: false
+            isTaskCompleted: false,
+            isTaskShowed: true
         }
     }
 }
